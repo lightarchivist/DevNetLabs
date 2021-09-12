ae48798d287269e49b75ee1ec43865806bad5a4c
lightarchivist <sebastian.szmytka@gmail.com>
2021-09-12 11:43:52 +0100

    pylink-run-1

--- firewalla.py
+++ firewalla.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 
-from netmiko import ConnectHandler, redispatch
-from getpass import getpass
-
-class netDevice
-string devName
-string devIP
-method remoteAccess
-
+class netDevice:
+    string devName
+    string devIP
+    method remoteAccess
