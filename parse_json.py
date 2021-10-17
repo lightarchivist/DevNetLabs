608bddeaf2a5c15ed048862ced7b61f30b610b29
lightarchivist <sebastian.szmytka@gmail.com>
2021-10-17 13:59:07 +0100

    update json files

--- parse_json.py
+++ parse_json.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """ simple code to parse json """
 import json
-with open("data.json", "r", encoding = "UTH-8") as file:
+with open("data.json", "r", encoding = "UTF-8") as file:
     data = json.load(file)
-    print(data)
+    print(data['node']['hostname'])
