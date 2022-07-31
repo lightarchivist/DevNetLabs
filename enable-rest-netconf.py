939073e2399be7be8248dc39857dd5170e111068
seb <sebastian.szmytka@gmail.com>
2022-07-31 22:52:58 +0100

    added IP validdation

--- enable-rest-netconf.py
+++ enable-rest-netconf.py
@@ -1,5 +1,14 @@
 """simple scrypt to enable netconf and restconf on lab devices"""
 from netmiko import ConnectHandler
+import ipaddress
+
+def validate_ipadd(address):
+    try:
+        ip = ipaddress.ip_address(address)
+        print = ("IP address is valid ".format(address, ip))
+    except ValueError:
+        print("IP address is not valid".format(address))
+
 
 with open("hosts.log", "r") as hosts:
     print(hosts.read())
